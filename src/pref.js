2fae5c190a4b6a297fa642e9e475e134f3c94556
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-06-01 23:23:49 +0300

    chore: update preferences

--- src/pref.js
+++ src/pref.js
@@ -2,11 +2,15 @@ import Adw from "gi://Adw";
 import GObject from "gi://GObject";
 import Gio from "gi://Gio";
 
+const themes = ["system", "light", "dark"];
+const endianness = ["le", "be"];
+const encodingMode = ["text_encoding", "ieee_754"];
+
 export const PrefDialog = GObject.registerClass(
   {
     GTypeName: "PrefDialog",
     Template: getResourceURI("pref.ui"),
-    InternalChildren: ["system", "dark", "light"],
+    InternalChildren: [...themes, ...endianness, ...encodingMode],
     Properties: {
       theme: GObject.ParamSpec.string(
         "theme",
@@ -15,12 +19,26 @@ export const PrefDialog = GObject.registerClass(
         GObject.ParamFlags.READWRITE,
         ""
       ),
+      endianness: GObject.ParamSpec.string(
+        "endianness",
+        "Endianness",
+        "Machine endianness",
+        GObject.ParamFlags.READWRITE,
+        ""
+      ),
+      encoding_mode: GObject.ParamSpec.string(
+        "encoding_mode",
+        "encoding-mode",
+        "Encoding Mode",
+        GObject.ParamFlags.READWRITE,
+        ""
+      ),
     },
   },
   class PrefDialog extends Adw.PreferencesDialog {
     constructor(options = {}) {
       super(options);
-      
+
       this.settings = Gio.Settings.new(pkg.name);
       this.settings.bind(
         "preferred-theme",
@@ -28,33 +46,57 @@ export const PrefDialog = GObject.registerClass(
         "theme",
         Gio.SettingsBindFlags.DEFAULT
       );
-
-      this.bind_property_full(
-        "theme",
-        this._system,
-        "active",
-        GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
-        (_, theme) => [true, theme === "system"],
-        (_, theme) => [theme, "system"]
+      this.settings.bind(
+        "endianness",
+        this,
+        "endianness",
+        Gio.SettingsBindFlags.DEFAULT
+      );
+      this.settings.bind(
+        "encoding-mode",
+        this,
+        "encoding_mode",
+        Gio.SettingsBindFlags.DEFAULT
       );
 
-      this.bind_property_full(
-        "theme",
-        this._light,
-        "active",
-        GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
-        (_, theme) => [true, theme === "light"],
-        (_, theme) => [theme, "light"]
-      );
+      for (const theme of themes) {
+        this.bind_property_full(
+          "theme",
+          this[`_${theme}`],
+          "active",
+          GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
+          (binding, currTheme) => [true, currTheme === theme],
+          (binding, active) => [active, theme]
+        );
+      }
 
-      this.bind_property_full(
-        "theme",
-        this._dark,
-        "active",
-        GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
-        (_, theme) => [true, theme === "dark"],
-        (_, theme) => [theme, "dark"]
-      );
+      for (const currEndianness of endianness) {
+        this.bind_property_full(
+          "endianness",
+          this[`_${currEndianness}`],
+          "active",
+          GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
+          (binding, pickedEndianness) => [
+            true,
+            pickedEndianness === currEndianness,
+          ],
+          (binding, active) => [active, currEndianness]
+        );
+      }
+
+      for (const mode of encodingMode) {
+        this.bind_property_full(
+          "encoding_mode",
+          this[`_${mode}`],
+          "active",
+          GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
+          (binding, currEncodingMode) => [
+            true,
+            currEncodingMode === mode,
+          ],
+          (binding, active) => [active, mode]
+        );
+      }
     }
   }
 );
